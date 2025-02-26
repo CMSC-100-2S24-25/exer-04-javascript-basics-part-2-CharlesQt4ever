801f1745c56c785ef8df07b55ca5a734ef5983a5
Charles <cpdelosreyes@up.edu.ph>
2025-02-26 12:07:34 +0800

    Updated the functionalities and create .gitignore

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ console.log(generateUniqueID("Tim", "Berners-Lee"));
 console.log("\nTest addAccount:");
 const user1 = ["Alan", "Turing", "aturing@w3c.com", 58];
 const user2 = ["Ted", "Nelson", "ted.n@w3c.com", 43];
-const user3 = ["John", "", "invalidemail", 17];
+const user3 = ["Charles", "", "malingemail", 14];
 
 console.log(addAccount(user1)); // true
 console.log(addAccount(user2)); // true
